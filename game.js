607c36a8bd3ccfe605d46b80b8fc0b6db7ae225f
mohd-amaan2909 <mohdamaan2909@gmail.com>
2025-07-29 23:53:16 +0530

    Tic-Tac-Toe game using HTML CSS & JS

---
+++ game.js
@@ -0,0 +1,45 @@
+let boxes = document.querySelectorAll(".box");
+let reset = document.querySelector(".reset");
+
+let turn0 = true;
+
+const winPatterns = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+boxes.forEach((box)=>{
+    box.addEventListener("click" , () => {
+        console.log("box was clicked");
+        if(turn0){
+            box.innerText = "O";
+            turn0 = false;
+        }
+        else{
+            box.innerText = "X";
+            turn0 = true;
+        }
+        box.disabled = true;
+
+        checkWinner ();
+    })
+})
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+    pos1val = boxes[pattern[0]].innerText;
+    pos2val = boxes[pattern[1]].innerText;
+    pos3val = boxes[pattern[2]].innerText;
+
+    if(pos1val != "" && pos2val != "" && pos3val != ""){
+        if(pos1val===pos2val&& pos2val === pos3val){
+            console.log("winner!");
+        }
+    }
+    }
+}
