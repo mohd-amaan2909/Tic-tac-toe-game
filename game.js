69bcd7de695d628c5b704dbcf23404869aec9fca
mohd-amaan2909 <mohdamaan2909@gmail.com>
2025-07-31 08:22:57 +0530

    Added a draw feature

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ let reset = document.querySelector(".reset");
 let newBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msg-container");
 let msg = document.querySelector("#msg");
+let msgDraw = document.querySelector(".msg-draw");
+
 
 let turn0 = true;
 
@@ -16,39 +18,83 @@ const winPatterns = [
   [0, 4, 8],
   [2, 4, 6]
 ];
+const disableBox = ()=>{
+     for (const box of boxes) {
+        box.disabled = true;
+     }
+};
+const enableBox = ()=>{
+     for (const box of boxes) {
+        box.disabled = false;
+        box.innerText= "";
+        msgContainer.classList.add("hide");
+     }
+};
+const isDraw = () => {
+   let  allFilled = true;
+
+   boxes.forEach((box) =>{
+    if(box.innerText == "")
+    {allFilled = false;}
+   });
+
+   if(allFilled && !isWinner){
+    console.log("Draw!");
+    document.querySelector(".draw-text").innerText = "It's a Draw!";
+    msgDraw.classList.remove("hide");
+
+   }
+
+}
+
 boxes.forEach((box)=>{
     box.addEventListener("click" , () => {
-        console.log("box was clicked");
         if(turn0){
             box.innerText = "O";
             turn0 = false;
+            box.style.color = "#F8E9E9"
         }
         else{
             box.innerText = "X";
             turn0 = true;
+            box.style.color = "red";
         }
         box.disabled = true;
 
         checkWinner ();
-    })
-})
+        isDraw();
+    });
+});  
+
+const resetBtn= () =>{
+    turn0= true;
+    enableBox();   
+};
+
+newBtn.addEventListener("click", resetBtn);
+reset.addEventListener("click", resetBtn);
+
 const checkWinner = () => {
     for (let pattern of winPatterns) {
-    pos1val = boxes[pattern[0]].innerText;
-    pos2val = boxes[pattern[1]].innerText;
-    pos3val = boxes[pattern[2]].innerText;
+let  pos1val = boxes[pattern[0]].innerText;
+let  pos2val = boxes[pattern[1]].innerText;
+let  pos3val = boxes[pattern[2]].innerText;
 
     if(pos1val != "" && pos2val != "" && pos3val != ""){
         if(pos1val===pos2val && pos2val === pos3val){
             console.log("winner!", pos1val);
+            showWinner (pos1val);
+            isWinner=true;
+            return;
         }
-         showWinner (pos1val);
-         return;
+         
     }
     
     }
-}
+};
 const showWinner = (winner) =>{
       msg.innerText = `Congratulations, Winner is ${winner}`;
       msgContainer.classList.remove("hide");
-}
+      disableBox();
+};
+
